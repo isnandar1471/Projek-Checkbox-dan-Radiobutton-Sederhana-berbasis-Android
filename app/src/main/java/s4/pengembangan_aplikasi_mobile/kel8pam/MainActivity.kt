c3d9af6eaf03f96536ee8a0700650aecefe60e2e
isnandar1471 <isnandar.1471@gmail.com>
2021-03-08 19:47:55 +0700

    membuat palikasi kosong

---
+++ app/src/main/java/s4/pengembangan_aplikasi_mobile/kel8pam/MainActivity.kt
@@ -0,0 +1,11 @@
+package s4.pengembangan_aplikasi_mobile.kel8pam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
